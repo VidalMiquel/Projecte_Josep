9592372198e0544ad6954d63854182b8a591ba19
Miquel Vidal <miquelvidalcortes@gmail.com>
2021-04-17 13:28:03 +0200

    darrer commit

--- src/dcola.adb
+++ src/dcola.adb
@@ -6,6 +6,7 @@ package body dcola is
       q: pnodo renames qu.p;
       p: pnodo renames qu.p;
    begin 
+      --El punter q i p apunten INICIALMENT  a NULL.
       q:= null;
       p:= null;   
    end cvacia;
@@ -17,6 +18,7 @@ package body dcola is
    function esta_vacia (qu: in cola) return boolean is
       q: pnodo renames qu.q;
    begin
+      --El q ha d'apuntar a NULL.
       return q = null;
    end esta_vacia;
    -----------------------------------------------------------------------------
@@ -43,12 +45,15 @@ package body dcola is
       p: pnodo renames qu.p;
       r: pnodo;
    begin
+      --Intància d'un node.
       r:= new nodo;
       r.all:=(x, null);
+      --Cas coa es buida.
       if p = null then
          p:=r;
          q:=r;
       else
+         --Cas coa conté almenys un element.
          p.sig:= r;
          p:=r;
       end if;
@@ -77,7 +82,7 @@ package body dcola is
    
    -----------------------------------------------------------------------------
    --Mètode que comprova si la coa, està formada per un i només un element.
-   --Degut al funcionament de les coes, tan el punter p(fina coa) com el 
+   --Degut al funcionament de les coes, tan el punter p(final coa) com el 
    --punter q (inici coa) NO han d'apuntar a null , és a dir, han d'apuntar 
    --a un node, i ambdós han d'apuntar al mateix. 
    function is_last_item (qu: in cola) return Boolean is
